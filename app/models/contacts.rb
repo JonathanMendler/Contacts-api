76f1a4c68128c0c89b00943ab84c3f0a67000de2
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-09 12:31:54 -0500

    Generated new contact file

--- app/models/contacts.rb
+++
@@ -1,9 +0,0 @@
-class Contact
-
-  def initialize(options)
-    @first_name = options[first_name]
-    @last_name = options[last_name]
-    @phone_number = options[phone_number]
-    @email = options[email]
-    @relationship = options[relationship]
-end
