973657315c90456c3ed97a7d8277d5bfdad17a9a
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-09 14:16:04 -0500

    Added jbuilder views to contacts-api

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,6 @@
 class ContactsController < ApplicationController
+  def one_contact
+    contact = Contact.first
+    render json: { message: "hi!" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/one_contact", controller: "contacts", action: "one_contact"
 end
