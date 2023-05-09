fea1f20dd6aa5eb2d91a54a4a81362fd835c3729
Jonathan Mendler <Jon.mendler@gmail.com>
2023-05-09 14:21:28 -0500

    modified jbuilder contact & contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,11 @@
 class ContactsController < ApplicationController
   def one_contact
-    contact = Contact.first
-    render json: { message: "hi!" }
+    @contact = Contact.first
+    render template: "contacts/show"
+  end
+
+  def all_contacts
+    @contacts = Contact.all
+    render template: "contacts/index"
   end
 end
